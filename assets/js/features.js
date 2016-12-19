875d98b55a3e9298edb5598dcd3b06b6ca7ad6c6
parkjjustin <parkjjustin@gmail.com>
2016-12-18 17:59:46 -0800

    Cleaned up functionality

--- assets/js/features.js
+++ assets/js/features.js
@@ -1,12 +1,3 @@
-
-// Off sets fixed navbar
-var shiftWindow = function() { scrollBy(0, -50) };
-if (location.hash) {
-    shiftWindow();
-};
-
-window.addEventListener("hashchange", shiftWindow);
-
 // Animates scroll in page anchor links
 $(document).ready(function(){
 	$('a[href^="#"]').on('click',function (event) {
@@ -23,15 +14,59 @@ $(document).ready(function(){
 	});
 });
 
-// Highlight links in anchor page
-$(function() {
-// When clicking on anchor links, this adds active class to highlight the link
-  $('nav a').click(function() {
-    $(this).addClass('active').siblings('nav a').removeClass('active');
-  });
 
-  $(document).scroll(function() {
-    $('nav a[href="#'+$('section:hover').attr('id')+'"]').addClass('active').siblings('nav a').removeClass('active');
-  });
-  
+// // Animates icons
+// var appear = function() {
+// 	var resize = $(".icon").animate({
+// 		height: "200px",
+// 		opacity: 1.0,
+// 		duration: "slow"
+// 	}, 700);
+// }
+
+// function appearTwo() {
+// 	setTimeout(appear, 200)
+// }
+
+// $(document).on("click",'#about-link', appearTwo)
+
+$(document).ready(function(){
+
+    /**
+     * This part handles the highlighting functionality.
+     * We use the scroll functionality again, some array creation and 
+     * manipulation, class adding and class removing, and conditional testing
+     */
+    var aChildren = $("nav li").children(); // find the a children of the list items
+    var aArray = []; // create the empty aArray
+    for (var i=0; i < aChildren.length; i++) {    
+        var aChild = aChildren[i];
+        var ahref = $(aChild).attr('href');
+        aArray.push(ahref);
+    } // this for loop fills the aArray with attribute href values
+
+    $(window).scroll(function(){
+        var windowPos = $(window).scrollTop() + 50; // "50px" is to offset for the fixed top nav
+        var windowHeight = $(window).height(); // get the height of the window
+        var docHeight = $(document).height();
+
+        for (var i=0; i < aArray.length; i++) {
+            var theID = aArray[i];
+            var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+            var divHeight = $(theID).height(); // get the height of the div in question
+            if (windowPos > divPos && windowPos < (divPos + divHeight)) {
+                $("a[href='" + theID + "']").addClass("nav-active");
+            } else {
+                $("a[href='" + theID + "']").removeClass("nav-active");
+            }
+        }
+
+        if(windowPos + windowHeight == docHeight) {
+            if (!$("nav li:last-child a").hasClass("nav-active")) {
+                var navActiveCurrent = $(".nav-active").attr("href");
+                $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
+                $("nav li:last-child a").addClass("nav-active");
+            }
+        }
+    });
 });
